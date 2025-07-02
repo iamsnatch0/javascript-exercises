fcffde20ddbf52a71d064e3244b3c0e8fba1c895
Milvin <44063014+iamsnatch0@users.noreply.github.com>
2025-07-02 22:32:53 +0100

    Revision Exercises Completed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(minValue, maxValue) {
+    if(!Number.isInteger(minValue)|| !Number.isInteger(maxValue)) return "ERROR";
+    if(minValue < 0 || maxValue < 0) return "ERROR";
+    let sum = 0;
+    for (let i = minValue; i <= maxValue; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
