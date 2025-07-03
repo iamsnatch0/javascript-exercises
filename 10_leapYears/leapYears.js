dc299e3a87fc56c5fb5e1f4307979197515afc4d
Milvin <44063014+iamsnatch0@users.noreply.github.com>
2025-07-03 18:47:43 +0100

    Revision Exercise Completed

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const yearDivideFour = year % 4 === 0;
+    const yearDivideCentury = year % 100 === 0;
+    const yearDivideFourHundred = year % 400 === 0;
+
+
+    if (
+        yearDivideFour && 
+        (!yearDivideCentury || yearDivideFourHundred)
+    ){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
